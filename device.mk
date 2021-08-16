roberto-sartori-gl/4.14-device-oneplus-cheeseburger

Include and use prebuilt 4.14 kernel
Hash: 85fa27fe2255

Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

device.mk
@@ -103,3 +103,8 @@ $(call inherit-product-if-exists, vendor/nxp/nxp-vendor.mk)
 
 # include board vendor blobs
 $(call inherit-product-if-exists, vendor/sony/yoshino-common/yoshino-partial.mk)
+
+LOCAL_KERNEL := device/oneplus/cheeseburger/kernel
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel

Beryllium-4-14-Project/device-sony-akari

device.mk
@@ -92,3 +92,8 @@ $(call inherit-product, device/xiaomi/sdm845-platform/platform.mk)
 
 # include board vendor blobs
 $(call inherit-product-if-exists, vendor/sony/tama-common/tama-partial.mk)
+
+LOCAL_KERNEL := device/xiaomi/beryllium/kernel
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel